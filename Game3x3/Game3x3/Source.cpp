e6d0d80d56f26b34fb88ece358c33569698dca18
litdarvin <litvints.dar@yandex.ru>
2016-09-30 12:12:18 +0300

    Game3x3
    Works slowly without checking solv.

--- Game3x3/Game3x3/Source.cpp
+++ Game3x3/Game3x3/Source.cpp
@@ -24,11 +24,10 @@ public:
 		zeroPosition = zeroPosition_;
 	}
 	
-	vector<shared_ptr<CVertex>> children;
-	weak_ptr<CVertex> parent;
-
+	vector<shared_ptr<CVertex>> Children;
+	weak_ptr<CVertex> Parent;
 	char GetWay() const { return wayToMe;  }
-	//weak_ptr<CVertex> GetParent() const { return parent; }
+
 	bool isItFinish() const
 	{
 		if( chain == 123456780 )
@@ -40,30 +39,28 @@ public:
 	{
 		int newChain;
 
-		if( zeroPosition + 3 <= 8 ) {
+		if( zeroPosition + 3 <= 8 && wayToMe != 'U' ) {
 			newChain = chain + ((int)pow( 10, 8 - zeroPosition ) - (int)pow( 10, 8 - zeroPosition - 3 ))* ReturnByIndex( zeroPosition + 3 );
-			children.push_back( shared_ptr<CVertex>( new CVertex( 'D', newChain, zeroPosition + 3 ) ) );
+			Children.push_back( shared_ptr<CVertex>( new CVertex( 'D', newChain, zeroPosition + 3 ) ) );
 
 		}
-		if( zeroPosition - 3 >= 0 ) {
+		if( zeroPosition - 3 >= 0 && wayToMe != 'D' ) {
 			newChain = chain + ((int)pow( 10, 8 - zeroPosition ) - (int)pow( 10, 8 - zeroPosition + 3 ))* ReturnByIndex( zeroPosition - 3 );
-			children.push_back( shared_ptr<CVertex>( new CVertex( 'U', newChain, zeroPosition - 3 ) ) );
+			Children.push_back( shared_ptr<CVertex>( new CVertex( 'U', newChain, zeroPosition - 3 ) ) );
 		}
-		if (zeroPosition != 2 && zeroPosition != 5 && zeroPosition != 8) {
+		if (zeroPosition != 2 && zeroPosition != 5 && zeroPosition != 8 && wayToMe != 'L' ) {
 			newChain = chain + ((int)pow( 10, 8 - zeroPosition ) - (int)pow( 10, 8 - zeroPosition - 1 ))* ReturnByIndex( zeroPosition + 1 );
-			children.push_back( shared_ptr<CVertex>( new CVertex( 'R', newChain, zeroPosition + 1 ) ) );
+			Children.push_back( shared_ptr<CVertex>( new CVertex( 'R', newChain, zeroPosition + 1 ) ) );
 		}
-		if (zeroPosition != 0 && zeroPosition != 3 && zeroPosition != 6) {
+		if (zeroPosition != 0 && zeroPosition != 3 && zeroPosition != 6 && wayToMe != 'R' ) {
 			newChain = chain + ((int)pow( 10, 8 - zeroPosition ) - (int)pow( 10, 8 - zeroPosition + 1 ))* ReturnByIndex( zeroPosition - 1 );
-			children.push_back( shared_ptr<CVertex>( new CVertex( 'L', newChain, zeroPosition - 1 ) ) );
+			Children.push_back( shared_ptr<CVertex>( new CVertex( 'L', newChain, zeroPosition - 1 ) ) );
 		}
 	}
 
 private:
-	
-
-	char wayToMe;
 	int chain;
+	char wayToMe;	
 	int zeroPosition;
 
 	int ReturnByIndex( int ind )
@@ -77,33 +74,35 @@ void bfs( shared_ptr<CVertex> graph )
 	queue<shared_ptr<CVertex>> Queue;
 	weak_ptr<CVertex> result;
 	Queue.push( graph );
+	bool stop = false;
 
-	while( !Queue.empty() )
+	while( !stop )
 	{
 		shared_ptr<CVertex> current = Queue.front();
 		Queue.pop();
 		current->CreateChildren();
 
-		for( auto it = current->children.begin(); it != current->children.end(); ++it )
+		for( auto it = current->Children.begin(); it != current->Children.end(); ++it )
 		{
-			(*it)->parent = current;
+			(*it)->Parent = current;
 
-			if (!(*it)->isItFinish()) {
+			if( !(*it)->isItFinish() ) {
 				Queue.push( *it );
 			} else {
 				result = *it;
+				stop = true;
 				break;
 			}
 		}
 	}
 
 	std::string resultString = "";
-	int resultCount = -1;
+	int resultCount = 0;
 
-	while( result.lock().use_count() != 0) {
+	while( result.lock()->GetWay() != '\0' ) {
 
 		resultString += result.lock()->GetWay();
-		result = result.lock()->parent;
+		result = result.lock()->Parent;
 		++resultCount;
 	}
 
@@ -115,19 +114,19 @@ void bfs( shared_ptr<CVertex> graph )
 int main()
 {
 	int chain = 0;
-	int zeroPosition = 0;
+	int zeroPosition = -1;
 
 	int current;
-	/*for (int i = 0; i < 9; ++i)
+
+	for (int i = 0; i < 9; ++i)
 	{
 		cin >> current;
-		if (current == 0) zeroPosition = i;
+		if( current == 0 ) zeroPosition = i;
+		if( zeroPosition != 0 )	chain = (int)pow( 10, i )*chain + current;
+		else chain = (int)pow( 10, i - 1 )*chain + current;
+	}
 
-		chain = (int)pow( 10, i )*chain + current;
-	}*/
-
-	cin >> chain;
-	shared_ptr<CVertex> graph( new CVertex( '/n', chain, zeroPosition ) );
+	shared_ptr<CVertex> graph( new CVertex( '\0', chain, zeroPosition ) );
 	bfs( graph );
 	system( "pause" );
 	return 0;

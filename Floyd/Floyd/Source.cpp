bd90edcd7a35d1b46990b306e0dc2f7dd25e3d61
DaryaLit <litv.dar@yandex.ru>
2016-10-01 22:07:15 +0300

    Floyd
    Passed

---
+++ Floyd/Floyd/Source.cpp
@@ -0,0 +1,46 @@
+//Автор: Литвинцева Дарья
+//Описание: задание 1, задача 3 на алгоритм Флойда-Уоршелла
+
+#include <iostream>
+#include <fstream>
+
+using std::fstream;
+
+void Floyd( int N, int** matrix )
+{
+	for( int i = 0; i < N; ++i)
+	{
+		for( int j = 0; j < N; ++j )
+		{
+			for( int k = 0; k < N; ++k ) matrix[j][k] = std::min(matrix[j][k], matrix[j][i] + matrix[i][k]);
+		}
+	}
+}
+
+int main()
+{
+	fstream input( "floyd.in", fstream::in );
+	fstream output( "floyd.out", fstream::out );
+
+	//reading
+	int N;
+	input>>N;
+
+	int** matrix = new int*[N];
+
+	for (int i = 0; i < N; ++i) 
+	{
+		matrix[i] = new int[N];
+		for (int j = 0; j < N; ++j)	input >> matrix[i][j];
+	}
+
+	Floyd( N, matrix );
+	
+	for (int i = 0; i < N; ++i) 
+	{
+		for (int j = 0; j < N; ++j)	output << matrix[i][j]<<" ";
+		output << std::endl;
+	}
+
+	return 0;
+}
